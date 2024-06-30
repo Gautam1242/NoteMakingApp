cd62796e011608777f42341bdf0fd75e1195a20c
vipax808 <sanskarverma982@gmail.com>
2024-06-30 19:45:30 +0530

    fix and update

--- index.js
+++ index.js
@@ -3,63 +3,82 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const { fileLoader } = require("ejs");
-//render our frontend
+
+// Set the view engine to ejs
 app.set("view engine", "ejs");
-//middlewares
+
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-//routes
-app.get("/", function (req, res) {
-  fs.readdir(`./files`, function (err, files) {
+// Routes
+
+// Home route: List all files
+app.get("/", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      console.error("Error reading files directory:", err);
+      return res.status(500).send("Server Error");
+    }
     res.render("index", { files: files });
   });
 });
 
-app.get("/file/:filename", function (req, res) {
-  fs.readFile(
-    `./files/${req.params.filename}`,
-    "utf-8",
-    function (err, filedata) {
-      res.render("show", { filename: req.params.filename, filedata: filedata });
-    }
-  );
-});
-
-app.get("/delete/:filename", function (req, res) {
-  // delete a file asynchronously
-  fs.unlink(`./files/${req.params.filename}`, (err) => {
+// View a file
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
     if (err) {
-      console.error(err);
-    } else {
-      res.redirect("/");
+      console.error(`Error reading file ${req.params.filename}:`, err);
+      return res.status(404).send("File Not Found");
     }
+    res.render("show", { filename: req.params.filename, filedata: filedata });
   });
 });
 
-app.get("/edit/:filename", function (req, res) {
+// Delete a file
+app.get("/delete/:filename", (req, res) => {
+  fs.unlink(`./files/${req.params.filename}`, (err) => {
+    if (err) {
+      console.error(`Error deleting file ${req.params.filename}:`, err);
+      return res.status(500).send("Server Error");
+    }
+    res.redirect("/");
+  });
+});
+
+// Edit a file: Show edit form
+app.get("/edit/:filename", (req, res) => {
   res.render("edit", { filename: req.params.filename });
 });
 
-app.post("/edit", function (req, res) {
-  fs.rename(
-    `./files/${req.body.previous}`,
-    `./files/${req.body.new}`,
-    function (err) {
-      res.redirect("/");
+// Edit a file: Handle form submission
+app.post("/edit", (req, res) => {
+  const { previous, new: newFilename } = req.body;
+  fs.rename(`./files/${previous}`, `./files/${newFilename}`, (err) => {
+    if (err) {
+      console.error(`Error renaming file from ${previous} to ${newFilename}:`, err);
+      return res.status(500).send("Server Error");
     }
-  );
+    res.redirect("/");
+  });
 });
 
-app.post("/create", function (req, res) {
-  fs.writeFile(
-    `./files/${req.body.title.split(" ").join("")}.txt`,
-    req.body.details,
-    function (err) {
-      res.redirect("/");
+// Create a new file
+app.post("/create", (req, res) => {
+  const { title, details } = req.body;
+  const filename = `${title.split(" ").join("")}.txt`;
+  fs.writeFile(`./files/${filename}`, details, (err) => {
+    if (err) {
+      console.error(`Error creating file ${filename}:`, err);
+      return res.status(500).send("Server Error");
     }
-  );
+    res.redirect("/");
+  });
 });
 
-app.listen(3000);
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
