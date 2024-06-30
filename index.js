2617ff5cea12eb722a2921a268d42709179ebd7f
Gautam Shah <gautamshah317@gmail.com>
2024-06-30 21:09:18 +0400

    port not working

--- index.js
+++ index.js
@@ -78,8 +78,7 @@ app.post("/create", (req, res) => {
 });
 
 // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-app.listen("https://gautam1242.github.io/NoteMakingApp/");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
