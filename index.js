123bf91ad5dc5d50ba9f1ba96ac3b9e1a2fedbc1
Gautam Shah <gautamshah317@gmail.com>
2024-06-30 21:01:07 +0400

    port fixing

--- index.js
+++ index.js
@@ -78,7 +78,8 @@ app.post("/create", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+app.listen("https://gautam1242.github.io/NoteMakingApp/");
